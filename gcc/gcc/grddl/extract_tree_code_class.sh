db6235b06ce46d08037fb34a09f3584a7f378613
Michael DuPont <JamesMikeDuPont@gmail.com>
2009-02-01 17:58:29 +0100

    gddrl from swig

--- gcc/gcc/grddl/extract_tree_code_class.sh
+++ gcc/gcc/grddl/extract_tree_code_class.sh
@@ -1,7 +1,9 @@
 grep  -B1 -A15 'enum tree_code_class {' ../tree.h > gcc_tree_code_class.h
 swig -xml -module tree_code_class   gcc_tree_code_class.h
 
-export URI=http://github.com/h4ck3rm1k3/gcc/blob/introspector_branch/gcc/gcc/grddl/swig_tree.h.enum.tree_code_class.owl
+
+#http://github.com/h4ck3rm1k3/gcc/blob/introspector_branch/gcc/gcc/grddl/swig_tree.h.enum.tree_code_class.owl
+export URI=http://purl.oclc.org/NET/INTROSPECTOR/gcc/enum/tree_code_class.owl
 
 #xsltproc --param ontology_date "'`date --rfc-3339=ns`'" --param ontology_uri "'http://github.com/h4ck3rm1k3/gcc/introspector_branch/gcc/gcc/grddl/swig_tree.h.enum.tree_code_class.owl'"  --param ontology_name "'gcc tree.h enum tree_code_class '" --param ontology_acronym "'GCCTTCS'"  extract_enum.xsl  gcc_tree_code_class_wrap.xml 
 
